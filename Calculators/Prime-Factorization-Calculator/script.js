7e34c3240b818ae4f63098ad656b45be074e4cd9
vrun1208 <vrun4750@gmail.com>
2024-01-09 18:30:38 +0530

    major changes in formatting

--- Calculators/Prime-Factorization-Calculator/script.js
+++ Calculators/Prime-Factorization-Calculator/script.js
@@ -8,6 +8,7 @@ function isPrime(input){
 }
 
 function getFactors(input){
+    document.getElementById('output').innerHTML = '';
     var input = document.getElementById("input").value;
     result = [];
 
@@ -18,14 +19,48 @@ function getFactors(input){
     else{
         for(let i=2; i<=input; i++){
             while(isPrime(i) && input%i == 0){
-                if(!result.includes(i)) result.push(i);
+                result.push(i);
                 input/=i;
             }
         }
     }
-    var out = result.toString();
 
-    document.getElementById('output').innerText = `${out}`;
+    const obj = {};
+    result.forEach(function(x){
+        obj[x] = (obj[x] || 0) +1;
+    });
+    //console.log(obj);
+    //const exponential_values = [];
+
+    let values = Object.keys(obj);
+
+    values.forEach((e, index) => {
+        //exponential_values.push(`${e} ${String.fromCharCode(0x2070 + obj[e])}`)
+        //document.getElementById('output').innerText = `${e} `
+        let exponent = obj[e];
+        let supElement = document.createElement('sup');
+        supElement.textContent = exponent;
+        supElement.className = "sup_id";
+
+        let spn = document.createElement('span');
+        spn.textContent = " " + ' X ';
+        spn.className = "spn";
+
+        let container = document.createElement('div');
+        container.textContent = e + '';
+        container.appendChild(supElement);
+        
+        let out = document.getElementById('output');
+        out.appendChild(container);
+
+        if(index < values.length -1){
+            container.appendChild(spn);
+        }
+        
+    })
+    //var out = exponential_values.toString();
+    //var res = out.replaceAll(',', ' X ');
+
 }
 
 
