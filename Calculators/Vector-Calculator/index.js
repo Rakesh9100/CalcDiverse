5119ddd4ab93c0322a023f2d508956a4f954bd0e
GhoshAditi <hena.ghosh786@gmail.com>
2024-01-20 00:03:18 +0530

    Added Vector Calculator

---
+++ Calculators/Vector-Calculator/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var ax,ay,bx,by;
+
+function radToDeg(rads) {
+	return rads * (180/Math.PI);
+}
+
+function calc() {
+	ax = parseInt(document.getElementById("ax").value);
+	ay = parseInt(document.getElementById("ay").value);
+	bx = parseInt(document.getElementById("bx").value);
+	by = parseInt(document.getElementById("by").value);
+	var cx = ax+bx;
+	var cy = ay+by;
+	var cxdot = ax*bx;
+	var cydot = ay*by;
+	var angle = radToDeg(Math.atan(cy/cx));
+	// A + B, angel = tan^-1(y/x)
+	var easyAnswerHighlight = '<div id="vectC" style="display:inline;"> \ <div id="vectCchild" style="display:inline; -moz-user-select: none; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -ms-user-select: none; user-select: none;"> \ Vector C = \ </div> \ <'+ cx + ", " + cy + '></div><br> \
+	<div id="vectCDot" style="display:inline;"> \ <div id="vectCDotchild" style="display:inline; -moz-user-select: none; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -ms-user-select: none; user-select: none;"> \ Vector C Dot = \ </div> \ <' + cxdot + ", " + cydot + '></div><br> \
+	<div id="angle" style="display:inline;"> \ <div id="angleChild" style="display:inline; -moz-user-select: none; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -ms-user-select: none; user-select: none;"> \ Angle = \ </div> \ '  + angle + '</div>';
+ 	document.getElementById("results").innerHTML =
+	easyAnswerHighlight;
+} 
