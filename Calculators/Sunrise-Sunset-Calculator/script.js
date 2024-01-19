4599ce96dd499095d1cc298f638692c30af1f878
Neeraj <neerajantil.antil26@gmail.com>
2024-01-19 23:53:26 +0530

    [ISSUE - 372] Adding Sunrise and Sunset Calculator

--- Calculators/Sunrise-Sunset-Calculator/script.js
+++ Calculators/Sunrise-Sunset-Calculator/script.js
@@ -5,6 +5,7 @@ const sunset = document.querySelector('#set-time');
 const button = document.querySelector('#button');
 
 async function getTimes() {
+  // Fetches the sunrise and sunset times from the API
   const response = await fetch(
     `https://api.sunrise-sunset.org/json?lat=${latitude.value}&lng=${longitude.value}&formatted=0`
   );
@@ -12,6 +13,7 @@ async function getTimes() {
 
   const { sunrise: rise, sunset: set } = data.results;
 
+  // Gets the time and set PM or AM accordingly
   sunrise.textContent = (new Date(rise).getHours() - 12 > 0 ? new Date(rise).getHours() - 12 : new Date(rise).getHours())+':'+new Date(rise).getMinutes()+' '+(new Date(rise).getHours() - 12 > 0 ? 'PM' : 'AM');
   sunset.textContent = (new Date(set).getHours() - 12 > 0 ? new Date(set).getHours() - 12 : new Date(set).getHours())+':'+new Date(set).getMinutes()+' '+ (new Date(set).getHours() - 12 > 0 ? 'PM' : 'AM');
 }
