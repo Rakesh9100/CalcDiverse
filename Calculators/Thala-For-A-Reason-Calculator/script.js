6911ce55b8aeb46edb4c339a3933092f2efcb0e7
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-02-03 17:08:44 +0530

    Update script.js

--- Calculators/Thala-For-A-Reason-Calculator/script.js
+++ Calculators/Thala-For-A-Reason-Calculator/script.js
@@ -2,7 +2,7 @@ function calculate() {
     let userInput = document.getElementById("userInput").value;
 
     // Check if the input is a valid 6-digit number
-    if (/^[1-9]\d{2}\d{3}$/.test(userInput)) {
+    if (/^\d{3}\d{3}$/.test(userInput)) {
         // Extract the first 3 digits
         let originalNumber = userInput.slice(0, 3);
 
@@ -23,12 +23,9 @@ function calculate() {
         // Display the result and calculation on the page
         document.getElementById("result").innerText = `The final answer is: ${finalResult.toFixed(0)}! Thala for a Reason!`;
         document.getElementById("calculation").innerText = `Calculations:\n${calculationText}`;
-    }else if (/^0\d{5}$/.test(userInput)) {
-         // Display an alert for an input starting with 0
-         alert("Please enter a six-digit number where the first digit is non-zero.");
     } else {
         // Display an error message
-        document.getElementById("result").innerText = "Invalid input, Thala. Please enter a 6-digit number where first digit is non-zero.";
+        document.getElementById("result").innerText = "Invalid input, Thala. Please enter a 6-digit number with the same 3 digits repeated.";
         document.getElementById("calculation").innerText = "Calculations: Invalid input.";
     }
 }
