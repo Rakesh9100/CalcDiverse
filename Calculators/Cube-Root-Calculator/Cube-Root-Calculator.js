fbe8029724842e7940e3efd8f90912b9f952ff96
Shivam Verma <98605616+shivamm-verma@users.noreply.github.com>
2024-02-03 19:25:47 +0530

    Added Cube Root Calculator

---
+++ Calculators/Cube-Root-Calculator/Cube-Root-Calculator.js
@@ -0,0 +1,14 @@
+function cubeRootCalc() {
+    const numb = document.getElementById("numb").value;
+    if (numb === "") {
+        alert("Please enter a Number");
+        return;
+    }
+    let cubeAns = (numb) ** (1 / 3);
+    // cubeAns = Math.pow(numb,(1/3));
+    let roundAns = cubeAns.toFixed(4);
+    const resultPara = document.querySelector(".res-stat");
+    resultPara.innerHTML = `<h3>Cube Root of number ${numb}= <h2>${roundAns}</h2></h3>`;
+    document.getElementById("numb").value = "";
+}
+
