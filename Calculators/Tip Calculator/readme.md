dfc6cb7fb2cd1898fc043301ba7ff956097dccb2
Nayanika1402 <132455412+Nayanika1402@users.noreply.github.com>
2024-05-10 18:39:15 +0530

    Files arranged and formatted with identation.

---
+++ Calculators/Tip Calculator/readme.md
@@ -0,0 +1,14 @@
+# <p align="center">Tip Calculator</p>
+
+## Description :-
+
+Calculates the total bill with the value of Bill Amount and Tip Percentage.
+
+## Tech Stacks :-
+
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots :-
+
---
+++ Calculators/Tip Calculator/script.js
@@ -0,0 +1,15 @@
+const btnE1 = document.getElementById("calculate");
+const billInput =document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const totalSpan = document.getElementById("total");
+
+function calculateTotal(){
+    const billValue = billInput.value;
+    const tipValue = tipInput.value;
+    const totalValue = billValue * (1 + tipValue / 100);
+    totalSpan.innerText = totalValue.toFixed(2);
+
+}
+
+
+btnE1.addEventListener("click", calculateTotal);
