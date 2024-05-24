3b5b17fbf4f1233180de997e1157b42a67c28e65
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-05-24 14:15:57 +0530

    Update and rename app.js to script.js

--- Calculators/Loan-Amount-Calculator/script.js
+++ Calculators/Loan-Amount-Calculator/script.js
@@ -1,10 +1,10 @@
 const calcForm = document.getElementById('calc-form'),
-      loanAmount = document.getElementById('loan-amount'),
-      interestRate = document.getElementById('interest-rate'),
-      noOfMonth = document.getElementById('no-of-month'),
-      calcBtn = document.getElementById('calc-btn'),
-      clearBtn = document.getElementById('clear-btn'),
-      paymentInfoList = document.querySelectorAll('.payment-info div span');
+    loanAmount = document.getElementById('loan-amount'),
+    interestRate = document.getElementById('interest-rate'),
+    noOfMonth = document.getElementById('no-of-month'),
+    calcBtn = document.getElementById('calc-btn'),
+    clearBtn = document.getElementById('clear-btn'),
+    paymentInfoList = document.querySelectorAll('.payment-info div span');
 
 calcForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,7 +14,7 @@ calcForm.addEventListener('submit', (e) => {
 clearBtn.addEventListener('click', clearInputAndResult);
 
 // show payment info
-function showPaymentInfo(){
+function showPaymentInfo() {
     let monthlyPayment = calcMonthlyPayment(loanAmount.value, interestRate.value, noOfMonth.value);
     paymentInfoList[0].innerHTML = `₹${loanAmount.value.toLocaleString()}`;
     paymentInfoList[1].innerHTML = `${interestRate.value}%`;
@@ -22,15 +22,15 @@ function showPaymentInfo(){
     paymentInfoList[3].innerHTML = `₹${parseFloat(monthlyPayment).toLocaleString()}`;
 }
 
-function calcMonthlyPayment(PV, i, n){
-    i = (i/100) / 12;
+function calcMonthlyPayment(PV, i, n) {
+    i = (i / 100) / 12;
     let PMT = (PV * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
     return PMT.toFixed(2);
 }
 
-function clearInputAndResult(){
+function clearInputAndResult() {
     calcForm.reset();
     paymentInfoList.forEach(item => {
-        item.innerHTML = "&mdash;";
+        item.innerHTML = "—";
     });
 }
