d7371c9f50362d9fb2a18ade63a5106b657911ab
unknown <jhashivangisingh03@gmail.com>
2024-05-24 13:03:23 +0530

    Functionalities of . changed

--- Calculators/Basic-Calculator/script.js
+++ Calculators/Basic-Calculator/script.js
@@ -4,6 +4,8 @@ let calc = document.forms["calc"];
 let txt = calc.elements["txt"];
 let math = window.math;
 let display = document.getElementById("display");
+// let zero = document.getElementById("display");
+
 
 document.addEventListener("keydown", function (event) {
     const validKeys = /^[0-9.\+\-\*\/\(\)\^\%\{\}\[\]&]$/;
@@ -46,3 +48,31 @@ function clearOne() {
     let currentValue = calc.txt.value;
     calc.txt.value = currentValue.slice(0, -1);
 }
+
+function point() {
+    let currentValue = calc.txt.value;
+    if (currentValue == "") {
+        calc.txt.value = '0' + '.';
+    }
+    else {
+
+        if (currentValue.charAt(currentValue.length - 1) == "." || currentValue.charAt(currentValue.length - 1) == "(") {
+            calc.txt.value = calc.txt.value + '0' + '.';
+            return;
+        }
+        if (currentValue.charAt(currentValue.length - 1) == "+" ||
+            currentValue.charAt(currentValue.length - 1) == "*" ||
+            currentValue.charAt(currentValue.length - 1) == "-" ||
+            currentValue.charAt(currentValue.length - 1) == "/" ||
+            currentValue.charAt(currentValue.length - 1) == "%" ||
+            currentValue.charAt(currentValue.length - 1) == "√" ||
+            currentValue.charAt(currentValue.length - 1) == "^") {
+            calc.txt.value = calc.txt.value + '0' + '.';
+            return;
+        }
+        else {
+            calc.txt.value = calc.txt.value + ".";
+            return;
+        }
+    }
+};
