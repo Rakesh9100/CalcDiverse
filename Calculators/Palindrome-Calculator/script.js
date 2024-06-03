85b7a892a00c40945f4a999fac142cbe9a4bc989
diyaaa19 <151644762+diyaaa19@users.noreply.github.com>
2024-06-04 01:39:27 +0530

    Update script.js

--- Calculators/Palindrome-Calculator/script.js
+++ Calculators/Palindrome-Calculator/script.js
@@ -2,10 +2,14 @@ function checkPalindrome() {
     var userInput = document.getElementById("input").value;
     var reversedInput = userInput.split('').reverse().join('');
     var isPalindrome = (userInput === reversedInput);
+    let resultDiv = document.querySelector('.result');
+    let formDiv = document.getElementById('calculatorForm');
+    formDiv.style.display = 'none'; 
+    resultDiv.style.display = '';
 
     if (isPalindrome) {
-        document.getElementById("result").innerText = userInput + " is a palindrome.";
+        document.getElementById("result").innerHTML =  ` ${userInput} is a palindrome.`;
     } else {
-        document.getElementById("result").innerText = userInput + " is not a palindrome.";
+        document.getElementById("result").innerHTML =` ${userInput} is not a palindrome.`;
     }
 }
