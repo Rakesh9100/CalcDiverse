24c3f9d3d16d3d4f74417f382f93d3af19badaa7
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-06-27 16:11:24 +0530

    Update script.js

--- Calculators/Caesar-Cipher-Encryption-Calculator/script.js
+++ Calculators/Caesar-Cipher-Encryption-Calculator/script.js
@@ -1,30 +1,30 @@
 function encrypt() {
-  const plaintext = document.getElementById('plaintext').value;
-  let key = parseInt(document.getElementById('key').value);
-  const ciphertext = document.getElementById('ciphertext');
-  
-  if (isNaN(key) || key < 0 || key > 25) {
-      alert('Key must be a number between 0 and 25.');
-      return;
-  }
+    const plaintext = document.getElementById('plaintext').value;
+    let key = parseInt(document.getElementById('key').value);
+    const ciphertext = document.getElementById('ciphertext');
 
-  let encryptedText = '';
+    if (isNaN(key) || key < 0 || key > 25) {
+        alert('Key must be a number between 0 and 25.');
+        return;
+    }
 
-  for (let i = 0; i < plaintext.length; i++) {
-      let char = plaintext[i];
+    let encryptedText = '';
 
-      if (char.match(/[a-z]/i)) {
-          const code = plaintext.charCodeAt(i);
+    for (let i = 0; i < plaintext.length; i++) {
+        let char = plaintext[i];
 
-          if ((code >= 65) && (code <= 90)) {
-              char = String.fromCharCode(((code - 65 + key) % 26) + 65);
-          } else if ((code >= 97) && (code <= 122)) {
-              char = String.fromCharCode(((code - 97 + key) % 26) + 97);
-          }
-      }
+        if (char.match(/[a-z]/i)) {
+            const code = plaintext.charCodeAt(i);
 
-      encryptedText += char;
-  }
+            if ((code >= 65) && (code <= 90)) {
+                char = String.fromCharCode(((code - 65 + key) % 26) + 65);
+            } else if ((code >= 97) && (code <= 122)) {
+                char = String.fromCharCode(((code - 97 + key) % 26) + 97);
+            }
+        }
 
-  ciphertext.value = encryptedText;
+        encryptedText += char;
+    }
+
+    ciphertext.value = encryptedText;
 }
