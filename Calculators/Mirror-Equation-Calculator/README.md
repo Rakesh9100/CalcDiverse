26ed46cc46bbd12e182f4ab66e89874172439e12
Anshika Yadav <14anshika7yadav@gmail.com>
2024-06-29 19:40:28 +0530

    Added Mirror Equation Calculator #1183

---
+++ Calculators/Mirror-Equation-Calculator/README.md
@@ -0,0 +1,15 @@
+# <p align="center">Mirror Equation Calculator</p>
+
+## Description :-
+
+Mirror Equation Calculator is a web application that calculates the focal length if type of mirror and image and object distance is given.
+
+## Tech Stacks :-
+
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots :-
+
+![image]()
---
+++ Calculators/Mirror-Equation-Calculator/script.js
@@ -0,0 +1,20 @@
+function calculateFocalLength() {
+    let mirrorType = document.getElementById('mirrorType').value;
+    let objectDistance = parseFloat(document.getElementById('objectDistanceInput').value);
+    let imageDistance = parseFloat(document.getElementById('imageDistanceInput').value);
+    let result = document.getElementById('result');
+
+    if (isNaN(objectDistance) || isNaN(imageDistance)) {
+        result.innerHTML = "Please enter valid numbers for object and image distances.";
+    } else {
+        let focalLength;
+
+        if (mirrorType === 'concave') {
+            focalLength = (objectDistance * imageDistance) / (objectDistance + imageDistance);
+        } else if (mirrorType === 'convex') {
+            focalLength = (objectDistance * imageDistance) / (objectDistance - imageDistance);
+        }
+
+        result.innerHTML = `Focal Length: ${focalLength.toFixed(2)} cm`;
+    }
+}
