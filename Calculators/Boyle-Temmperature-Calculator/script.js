a48de3b5ebd291995ef4b021f423e3873bf3f5b6
Ishita Mukherjee <ishitam299@gmail.com>
2024-07-15 23:33:16 +0530

    Update script.js

--- Calculators/Boyle-Temmperature-Calculator/script.js
+++ Calculators/Boyle-Temmperature-Calculator/script.js
@@ -1,18 +1,28 @@
 
 
-// Function to calculate the distance
 function calculateTemperature() {
     // Get input values
     var a = parseFloat(document.getElementById('a').value);
     var b = parseFloat(document.getElementById('b').value);
-
-   console.log("hii");
-    // Calculate distance
-    var temperature = a/(b*8.314);
-    var temperature1 = a/(b*0.0821);
-
-    // Display the result
-    document.getElementById('result').innerHTML = 'Boyle Temperature (When R = 8.314 J.K^(-1).mol^(-1)): ' + temperature +' and Boyle Temperature (When R = 0.0821 L.atm.K^(-1).mol^(-1)): ' + temperature1;
+  
+    // Check for empty inputs
+    if (isNaN(a) || a === 0) {
+      alert("Please enter a valid value for a");
+    } else if (isNaN(b) || b === 0) {
+      alert("Please enter a valid value for b");
+    } else {
+      // Calculate temperatures with correct units
+      var temperature = a / (b * 8.314); // in Kelvin
+      var temperature1 = a / (b * 0.0821); // in Kelvin
+  
+      // Round outputs to 2 decimal places
+      temperature = temperature.toFixed(2);
+      temperature1 = temperature1.toFixed(2);
+  
+      // Display results
+      document.getElementById('result').innerHTML = 'Boyle Temperature (When R = 8.314 J.K^(-1).mol^(-1)): ' + temperature + ' K and Boyle Temperature (When R = 0.0821 L.atm.K^(-1).mol^(-1)): ' + temperature1 + ' K';
+  
+    }
 }
 
 // Function to reset the form
