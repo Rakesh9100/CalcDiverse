00dbe2ee5006a22fd3438f5c22594869e982f6f3
meetarora <meet.arora2403@gmail.com>
2024-07-17 10:55:25 +0530

    iso-commit

---
+++ Calculators/Isogram-Calculator/README.md
@@ -0,0 +1,16 @@
+# <p align="center">Isogram Calculator</p>
+
+## Description :-
+
+A simple web-based calculator to find whether the string is Isogram or not.<br>
+**Calculator logic:** An isogram is a word in which no letter occurs more than once.
+
+## Tech Stacks :-
+
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots :-
+![image](https://github.com/user-attachments/assets/0323fc2e-1698-4103-b16c-ac62900ef7fa)
+
---
+++ Calculators/Isogram-Calculator/script.js
@@ -0,0 +1,38 @@
+function isIsogram(str) {
+    let lowerStr = str.toLowerCase();
+    let length = lowerStr.length;
+    let count = 0;
+    for(let i=0;i<length;i++){
+        for(let j=i+1;j<length;j++){
+            if(lowerStr[i]==lowerStr[j]){
+                return false;
+            }
+        }
+    }
+    return true;
+    // let charCount = {};
+  
+    // for (let char of lowerStr) {
+    //   if (charCount[char]) {
+    //     charCount[char]++;
+    //   } else {
+    //     charCount[char] = 1;
+    //   }
+    // }
+  
+    // let counts = Object.values(charCount);
+    // return counts.every(count => count === counts[0]);
+  }
+  
+  function checkIsogram() {
+    let string = document.getElementById("string").value;
+    let outputText = document.getElementById("output-text");
+  
+    if (isIsogram(string)) {
+      outputText.textContent = `"${string}" is an isogram.`;
+      outputText.style.color = 'green';
+    } else {
+      outputText.textContent = `"${string}" is not an isogram.`;
+      outputText.style.color = 'darkred';
+    }
+  }
