121ee6887d67c0c6e1c0599e3b83831eb364b951
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-07-30 08:45:13 +0530

    Update script.js

--- Calculators/Interplanetary-Weight-Calculator/script.js
+++ Calculators/Interplanetary-Weight-Calculator/script.js
@@ -1,8 +1,7 @@
-
 function calculateWeight() {
     const weightEarth = parseFloat(document.getElementById('weight').value);
     const planet = document.getElementById('planet').value;
-    if (isNaN(weightEarth) ) {
+    if (isNaN(weightEarth)) {
         document.getElementById('resultText').innerText = "Please enter a value!!";
         return;
     }
@@ -19,20 +18,14 @@ function calculateWeight() {
         neptune: 11.15
     };
 
-   
     const weightPlanet = weightEarth * gravitationalAcc[planet];
-    const weightFormatted = weightPlanet.toFixed(2); 
+    const weightFormatted = weightPlanet.toFixed(2);
 
     document.getElementById('resultText').textContent = `Weight on ${planet.charAt(0).toUpperCase() + planet.slice(1)}: ${weightFormatted} N`;
-
-   
-    
 }
-
 
 function clearForm() {
     document.getElementById('weight').value = '';
     document.getElementById('planet').value = 'earth';
     document.getElementById('resultText').textContent = '';
-   
 }
