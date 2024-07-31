92717a86031bc93417913015e06005afae428fa0
Tina <tinaborundia23@gmail.com>
2024-07-31 08:30:17 +0530

    Update Calci

--- Calculators/UV-Index-Calculator/script.js
+++ Calculators/UV-Index-Calculator/script.js
@@ -1,5 +1,5 @@
 function calculateProtection() {
-<<<<<<< HEAD
+  document.getElementById("result").innerText = protectionMessage;
   const uvIndexInput = document.getElementById("uvIndex").value;
   let protectionMessage = "";
 
@@ -33,39 +33,4 @@ function calculateProtection() {
   }
 
   document.getElementById("result").innerText = protectionMessage;
-=======
-    const uvIndexInput = document.getElementById("uvIndex").value;
-    let protectionMessage = "";
-
-    // Check if input is empty
-    if (uvIndexInput === "") {
-        protectionMessage = "Please enter a UV Index value.";
-    } else {
-        const uvIndex = parseFloat(uvIndexInput);
-
-        // Check if input is a valid number
-        if (isNaN(uvIndex)) {
-            protectionMessage = "Invalid UV Index. Please enter a numeric value.";
-        } else {
-            // Determine protection message based on UV Index
-            if (uvIndex < 3) {
-                protectionMessage = "Low risk. No protection needed.";
-            } else if (uvIndex >= 3 && uvIndex < 6) {
-                protectionMessage = "Moderate risk. Wear sunglasses and sunscreen.";
-            } else if (uvIndex >= 6 && uvIndex < 8) {
-                protectionMessage = "High risk. Cover up and wear sunscreen.";
-            } else if (uvIndex >= 8 && uvIndex < 11) {
-                protectionMessage =
-                    "Very high risk. Take extra precautions, cover up, and wear sunscreen.";
-            } else if (uvIndex >= 11) {
-                protectionMessage =
-                    "Extreme risk. Avoid the sun and take all protective measures.";
-            } else {
-                protectionMessage = "Invalid UV Index.";
-            }
-        }
-    }
-
-    document.getElementById("result").innerText = protectionMessage;
->>>>>>> c0bfef2ec841d1cc556f4ca24c210c418000e46c
 }
