fe45f68df83e45548c81f9221788c9fea53433a0
Anjaliavv51 <154777864+Anjaliavv51@users.noreply.github.com>
2024-08-03 15:06:01 +0530

    Updated progress code for all pages
    #1695

--- assets/about/about.js
+++
@@ -1,9 +0,0 @@
-  // Progress JS
-  window.onscroll = function() {myFunction()};
-
-  function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-  }
