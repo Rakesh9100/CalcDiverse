aca9a5c176625e9bd1f97682c1ec5b6208a05de1
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-08-03 20:35:46 +0530

    Update script.js

--- Calculators/Harmonic-Progression-Calculator/script.js
+++ Calculators/Harmonic-Progression-Calculator/script.js
@@ -10,36 +10,37 @@ function calculateHP() {
     }
     let result;
     if (calc === 'nthTerm') {
-        result = CalcNthTerm(a,d,n)[1];
+        result = CalcNthTerm(a, d, n)[1];
     } else if (calc === 'sumOfTerms') {
-        result = sumOfTerms(a,d,n)[1];
-    } else if(calc === 'harmonicMean'){
-        result = harmMean(a,d,n);
+        result = sumOfTerms(a, d, n)[1];
+    } else if (calc === 'harmonicMean') {
+        result = harmMean(a, d, n);
     }
 
-    showResult(result);    
+    showResult(result);
 }
 
 function showError(message) {
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = `<div class="error">${message}</div>`;
 }
-function CalcNthTerm(a,d,n){
+
+function CalcNthTerm(a, d, n) {
     const nth = 1 / ((1 / a) + (n - 1) * (1 / d));
-    return [nth,`The ${n}th term of the harmonic progression is: ${nth.toFixed(4)}`]
+    return [nth, `The ${n}th term of the harmonic progression is: ${nth.toFixed(4)}`]
 }
 
-function sumOfTerms(a,d,n){
-    var sumTerms=0;
-    for(i=1;i<=n;i++){
-        ith=CalcNthTerm(a,d,i)[0];
-        sumTerms+=ith;
+function sumOfTerms(a, d, n) {
+    var sumTerms = 0;
+    for (i = 1; i <= n; i++) {
+        ith = CalcNthTerm(a, d, i)[0];
+        sumTerms += ith;
     }
-    return [sumTerms,`The sum of first ${n} terms of the harmonic progression is: ${sumTerms.toFixed(4)}`];
+    return [sumTerms, `The sum of first ${n} terms of the harmonic progression is: ${sumTerms.toFixed(4)}`];
 }
 
-function harmMean(a,d,n){
-    mean=n/(sumOfTerms(a,d,n)[0]);
+function harmMean(a, d, n) {
+    mean = n / (sumOfTerms(a, d, n)[0]);
     return `The harmonic mean of first ${n} terms of the harmonic progression is: ${mean.toFixed(4)}`
 }
 
