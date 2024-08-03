a00ed8c1cc400184e1e6bb2e2fcff33c1b176b54
Anjaliavv51 <154777864+Anjaliavv51@users.noreply.github.com>
2024-08-03 14:58:33 +0530

    Removed code for faq and contact page
    #1695

--- assets/contact/contact.js
+++ assets/contact/contact.js
@@ -62,10 +62,3 @@ function validateEmail(emailFieldId) {
         emailField.setCustomValidity("");
     }
 }
-  // Progress JS
-  window.addEventListener('scroll', () => {
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    document.querySelector('.progress-bar').style.width = scrolled + '%';
-  });
