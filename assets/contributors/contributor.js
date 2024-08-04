faee2540af58185887538745109f1cdc881b2776
meetarora <meet.arora2403@gmail.com>
2024-08-04 09:52:35 +0530

    new-commit

--- assets/contributors/contributor.js
+++ assets/contributors/contributor.js
@@ -1,3 +1,9 @@
+const updateProgress = () =>{
+    const {scrollTop,scrollHeight} = document.documentElement;
+    const scrollPercent = `${(scrollTop/(scrollHeight-window.innerHeight)) * 100}%`;
+    document.querySelector('#progress-bar').style.setProperty('--progress',scrollPercent);
+}
+document.addEventListener('scroll',updateProgress);
 const hamBurger = document.querySelector(".hamburger");
 const nMenu = document.querySelector(".nav-menu");
 
