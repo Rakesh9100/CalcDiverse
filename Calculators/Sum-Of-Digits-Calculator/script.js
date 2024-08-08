13b4bc6d359b6463a9c3cade0b69fc532da21c77
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-08-08 20:24:00 +0530

    Update and rename sum.js to script.js

--- Calculators/Sum-Of-Digits-Calculator/script.js
+++ Calculators/Sum-Of-Digits-Calculator/script.js
@@ -1,42 +1,33 @@
 const txtInput = document.querySelector(".inputs input"),
-checkBtn = document.querySelector(".inputs button"),
-infoTxt = document.querySelector(".info-txt");
+    checkBtn = document.querySelector(".inputs button"),
+    infoTxt = document.querySelector(".info-txt");
 let filterInput;
 
 
-
 function isString(value) {
-	return typeof value === 'string' || value instanceof String;
+    return typeof value === 'string' || value instanceof String;
 }
 
 function sumDigits(value) {
+    sum = 0;
 
-        sum = 0;
-
-  while (value) {
-      sum += value % 10;
-      value = Math.floor(value / 10);
-  }
-	return sum;
+    while (value) {
+        sum += value % 10;
+        value = Math.floor(value / 10);
+    }
+    return sum;
 }
 
 checkBtn.addEventListener("click", () => {
     let n = filterInput;
-    console.log(typeof n);
-
-
     infoTxt.style.display = "block";
-    // if(isString(n)===true){
-    //     return infoTxt.innerHTML = `Enter a valid input`
-    // }
-    // else{
     infoTxt.innerHTML = `Sum Of Digits of <span>'${txtInput.value}'</span> is <span>'${sumDigits(n)}'</span>`;
 
 });
 
 txtInput.addEventListener("keyup", () => {
     filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
-    if(filterInput) {
+    if (filterInput) {
         return checkBtn.classList.add("active");
     }
     infoTxt.style.display = "none";
