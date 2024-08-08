3eb0811dd59369d4a0a24488ed6c9b9b0ea184d4
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-08-09 00:01:42 +0530

    Update script.js

--- Calculators/Lottery-Odds-Calculator/script.js
+++ Calculators/Lottery-Odds-Calculator/script.js
@@ -1,13 +1,18 @@
 function calculate() {
     // Get input values
-    const tickets = document.getElementById('tickets').value;
-    const sold = document.getElementById('sold').value;
+    const tickets = parseFloat(document.getElementById('tickets').value);
+    const sold = parseFloat(document.getElementById('sold').value);
 
-    // Perform the selected calculation
-    let r;
-    r = tickets/sold;
-    r = r*100;
-    let result= r.toString();
+    // Validate inputs
+    if (isNaN(tickets) || isNaN(sold) || sold <= 0) {
+        document.getElementById('result').textContent = 'Please enter the values';
+        return;
+    }
+
+    // Perform the calculation
+    let r = tickets / sold;
+    r = r * 100;
+    let result = r.toFixed(2); // Limit to two decimal places
     result += "%";
 
     // Display the result
